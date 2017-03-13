31ecb8ded86072b9e66dae02003119176ceeea11
syota nakamura <tar0heart@icloud.com>
2017-03-13 11:06:38 +0900

    オブジェクト指向1-3の課題を提出します

---
+++ CampChallenge_java/005.オブジェクト指向１/Human.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tar0
+ */
+public class Human {
+    public String name="";
+    public int age=0;
+    public void setHuman(String name, int age){
+    System.out.print(name+age);}
+    
+}
+
